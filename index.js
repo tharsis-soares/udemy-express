45a445231213b4fb57cc875c10eede5e25e290bb
tharsis-soares <50808840+tharsis-soares@users.noreply.github.com>
2021-03-05 20:57:02 -0300

    express-2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app = express()
+
+app.use('/opa', (req, res) => {
+    res.send('Estou bem!! Caramelo Surf')
+})
+
+app.listen(3000, () => {
+    console.log('Backend executando...2')
+})
